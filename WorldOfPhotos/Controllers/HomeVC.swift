2e506363fcda6981a9ab694b147659dd2da8e918
vamsi krishna reddy kamjula <krishnareddy0421@gmail.com>
2017-11-07 21:31:35 -0500

    Side Menus Implemented

---
+++ WorldOfPhotos/Controllers/HomeVC.swift
@@ -0,0 +1,35 @@
+//
+//  HomeVC.swift
+//  WorldOfPhotos
+//
+//  Created by vamsi krishna reddy kamjula on 11/7/17.
+//  Copyright © 2017 kvkr. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    // MARK: - Outlets
+    @IBOutlet weak var profileBtn: UIButton!
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpSideMenus()
+    }
+    
+    func setUpSideMenus() {
+        if revealViewController() != nil {
+            profileBtn.addTarget(revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+            revealViewController().rearViewRevealWidth = 200
+            revealViewController().rightViewRevealWidth = 200
+            
+            menuBtn.addTarget(revealViewController(), action: #selector(SWRevealViewController.rightRevealToggle(_:)), for: .touchUpInside)
+            
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            
+        }
+    }
+}
