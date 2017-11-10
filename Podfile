235aaf6e63d8d3abf0785decbbb9be2ba6a83242
vamsi krishna reddy kamjula <krishnareddy0421@gmail.com>
2017-11-10 13:26:39 -0500

    IQKeyboard Added To Login/SignUp Views

--- Podfile
+++ Podfile
@@ -4,5 +4,6 @@ target 'WorldOfPhotos' do
   use_frameworks!
 
   pod 'paper-onboarding', '~> 2.0.1'
+  pod 'IQKeyboardManagerSwift', '5.0.0'
 
 end
--- WorldOfPhotos/Controllers/Login-SignUpVC.swift
+++ WorldOfPhotos/Controllers/Login-SignUpVC.swift
@@ -13,7 +13,23 @@ class Login_SignUpVC: UIViewController {
     // MARK: - Outlets
     @IBOutlet weak var loginSignUpTitleLbl: UILabel!
     @IBOutlet weak var signUpStackView: UIStackView!
+    @IBOutlet weak var signUpBtn: UIButton!
+    @IBOutlet weak var loginStackView: UIStackView!
     @IBOutlet weak var loginSignUpBtn: UIButton!
+    
+    // MARK: - Login Outlets
+    @IBOutlet weak var loginEmailTxt: UITextField!
+    @IBOutlet weak var loginPasswordTxt: UITextField!
+    
+    
+    // MARK: - SignUp Outlets
+    @IBOutlet weak var firstNameTxt: UITextField!
+    @IBOutlet weak var middleNameTxt: UITextField!
+    @IBOutlet weak var lastNameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var phoneNumTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var confirmPasswordTxt: UITextField!
     
     var titleString: String?
     
@@ -30,20 +46,52 @@ class Login_SignUpVC: UIViewController {
         if loginSignUpTitleLbl.text == "Login" {
             UIView.animate(withDuration: 0.4, animations: {
                 self.signUpStackView.alpha = 0
+                self.signUpBtn.alpha = 1
+                self.loginStackView.alpha = 1
                 self.loginSignUpBtn.setTitle("Login", for: .normal)
             })
         } else {
             UIView.animate(withDuration: 0.4, animations: {
                 self.signUpStackView.alpha = 1
+                self.signUpBtn.alpha = 0
+                self.loginStackView.alpha = 0
                 self.loginSignUpBtn.setTitle("SignUp", for: .normal)
             })
         }
     }
     
     func handleKeyboardOnTap() {
-        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        view.addGestureRecognizer(tapGesture)
     }
     
+    @objc func handleTap() {
+        view.endEditing(true)
+    }
+    
+    @IBAction func signUpBtnPressed(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.4) {
+            self.loginStackView.alpha = 0
+            self.signUpStackView.alpha = 1
+            self.signUpBtn.alpha = 0
+            self.loginSignUpTitleLbl.text = "SignUp"
+            self.loginSignUpBtn.setTitle("SignUp", for: .normal)
+        }
+    }
+    
+    @IBAction func loginSignUpBtnPressed(_ sender: UIButton) {
+        if sender.titleLabel?.text == "SignUp" {
+            guard let fName = firstNameTxt.text, let mName = middleNameTxt.text, let lName = lastNameTxt.text, let email = emailTxt.text, let phone = phoneNumTxt.text, let pass = passwordTxt.text, let confPass = confirmPasswordTxt.text, firstNameTxt.text != "", lastNameTxt.text != "", emailTxt.text != "", phoneNumTxt.text != "", passwordTxt.text != "", confirmPasswordTxt.text != nil, passwordTxt.text == confirmPasswordTxt.text else {
+                // error handling
+                return
+            }
+            print("\(fName)+\(mName)+\(lName)+\(email)+\(phone)+\(pass)+\(confPass)")
+            
+        } else {
+            print("Print signup")
+        }
+    }
+
     @IBAction func backBtnPressed(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
